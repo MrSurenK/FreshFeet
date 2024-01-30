f3462f97a74cc7aff8aaf4b1106fb75f06c4b73c
MrSurenK <surenkworkz@gmail.com>
2024-01-30 23:30:49 +0800

    ToDo: link variation to variation option

--- backend/src/main/java/com/freshfeet/backend/model/VariationOption.java
+++ backend/src/main/java/com/freshfeet/backend/model/VariationOption.java
@@ -3,7 +3,11 @@ package com.freshfeet.backend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -11,6 +15,24 @@ public class VariationOption {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    private String value; //Value can be a shirt size or shoe size or colour
+
+    //Getters and Setters
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+
+
 
 
 }
