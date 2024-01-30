34c4b0cea52e16602975ebbcc720f319a02a9a2b
MrSurenK <surenkworkz@gmail.com>
2024-01-30 18:32:18 +0800

    Self-join established.

--- backend/src/main/java/com/freshfeet/backend/model/ProductCategory.java
+++ backend/src/main/java/com/freshfeet/backend/model/ProductCategory.java
@@ -1,22 +1,32 @@
 package com.freshfeet.backend.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.util.Lazy;
+
+import java.util.Set;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 public class ProductCategory {
 
     @Id
     @GeneratedValue
-    private Long categoryId;
+    private Long id;
 
     private String category;
 
+    @ManyToOne(fetch=LAZY)
+    @JoinColumn(name="fk_parent_cat")
+    private ProductCategory parentCategory;
 
-    public Long getCategoryId(){
-        return this.categoryId;
+    @OneToMany(mappedBy = ProductCategory_.PARENT_CATEGORY)
+    private Set<ProductCategory> subCategory;
+
+
+    public Long getId(){
+        return this.id;
     }
 
     public String getCategory(){
@@ -27,7 +37,19 @@ public class ProductCategory {
         this.category = category;
     }
 
+    public ProductCategory getParentCategory(){
+        return this.parentCategory;
+    }
 
+    public void setParentCategory(ProductCategory parentCategory){
+        this.parentCategory = parentCategory;
+    }
+
+    public Set<ProductCategory>getSubCategory(){
+        return subCategory;
+    }
+
+    //BiDirectional Mapping for parent and sub-cat
 
 
 }
