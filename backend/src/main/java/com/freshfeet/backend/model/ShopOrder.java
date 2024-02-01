f2a618e1f982a77eaea24a01b8909ed3e351982b
MrSurenK <surenkworkz@gmail.com>
2024-02-01 21:43:10 +0800

    Custom order id generator configured

--- backend/src/main/java/com/freshfeet/backend/model/ShopOrder.java
+++ backend/src/main/java/com/freshfeet/backend/model/ShopOrder.java
@@ -1,5 +1,6 @@
 package com.freshfeet.backend.model;
 
+import com.freshfeet.backend.util.CustomOrderId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -8,7 +9,25 @@ public class ShopOrder {
 
     //Custom order id (receipt)
     @Id
+    @CustomOrderId(name="order_no", prefix = "order_")
     private String orderId;
+
+
+
+
+
+
+
+
+
+
+    //Setters and Getters
+    public String getOrderId(){
+        return this.orderId;
+    }
+
+
+
 
 
 
--- backend/src/main/java/com/freshfeet/backend/util/CustomShopOrderGenerator.java
+++ backend/src/main/java/com/freshfeet/backend/util/CustomShopOrderGenerator.java
@@ -10,8 +10,12 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CustomShopOrderGenerator implements IdentifierGenerator {
+//    ToDO: Test the generator and ensure that the desired id is generated in MySQL
+    private String prefix;
+    private Integer nextId;
 
-    private static String prefix;
+    private Integer increment;
+
 
     public Serializable generate(
             SharedSessionContractImplementor session,
@@ -19,12 +23,15 @@ public class CustomShopOrderGenerator implements IdentifierGenerator {
     ) throws HibernateException {
 
 
-        // Access class field via reflection
+        // Access entity field via reflection(Object refers to entity object/domain object)
         for (Field field: object.getClass().getDeclaredFields()){
             field.setAccessible(true);
             if(field.isAnnotationPresent(CustomOrderId.class) && field.getType().equals(String.class)){
+                //getAnnotations looks for the specific annotation and allows me to access the annotation class fields/values
                 CustomOrderId annotation = field.getAnnotation(CustomOrderId.class);
                 prefix = annotation.prefix();
+                nextId = annotation.startWith();
+                increment = annotation.incrementBy();
                 break;
             }
         }
@@ -32,10 +39,10 @@ public class CustomShopOrderGenerator implements IdentifierGenerator {
         String sql = String.format("SELECT MAX(CAST(SUBSTRING(order_id, LENGTH('%s') + 1) AS UNSIGNED)) FROM shop_order WHERE order_id LIKE '%s%%'", prefix, prefix);
         Object result = session.createNativeQuery(sql,Integer.class).uniqueResult();
 
-        int nextId = 50;
+
             if(result!=null){
                 String maxId = result.toString();
-                nextId = Integer.parseInt(maxId) + 50;
+                nextId = Integer.parseInt(maxId) + increment;
             }
 
         return  prefix +
