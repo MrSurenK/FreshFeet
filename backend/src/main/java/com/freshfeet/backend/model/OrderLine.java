d635d5da0b17cbeafd484cfb12c0b4a4f11ec67f
MrSurenK <surenkworkz@gmail.com>
2024-02-03 17:35:42 +0800

    Setters and getters for itemized columns in OrderLine done. ToDo: links for OrderLine

--- backend/src/main/java/com/freshfeet/backend/model/OrderLine.java
+++ backend/src/main/java/com/freshfeet/backend/model/OrderLine.java
@@ -1,9 +1,8 @@
 package com.freshfeet.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
 
 @Entity
 public class OrderLine {
@@ -11,6 +10,36 @@ public class OrderLine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+
+    private Integer quantity;
+
+    @Column(precision = 8, scale = 2)
+    private BigDecimal price;
+
+
+    //Setters and Getters
+    public Long getId(){
+        return this.id;
+    }
+
+    public Integer getQuantity(){
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
 
 
 
