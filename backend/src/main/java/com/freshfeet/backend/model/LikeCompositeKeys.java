6f30579e29499f31cae74497f77a944b87368691
MrSurenK <surenkworkz@gmail.com>
2024-02-04 16:07:51 +0800

    ToDo: Configure Thymeleaf in Spring project and start creating APIs

--- backend/src/main/java/com/freshfeet/backend/model/LikeCompositeKeys.java
+++ backend/src/main/java/com/freshfeet/backend/model/LikeCompositeKeys.java
@@ -36,9 +36,12 @@ public class LikeCompositeKeys implements Serializable {
         if(o == null || getClass() != o.getClass()){
             return false;
         }
-
+        // Since method has determined that o is not null and is of the same class as the current instance
+        // We can safely cast o to LikeCompositeKeys type (Type Casting)
+        // We need to cast it to compare the specific fields of LikeCompositeKeys
         LikeCompositeKeys keys = (LikeCompositeKeys)o;
 
+        //current instance is compared with passed instance ('keys')
         return Objects.equals(userAccount, keys.userAccount) &&
                 Objects.equals(productItem, keys.productItem);
     }
