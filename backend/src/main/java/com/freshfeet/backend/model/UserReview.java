0d96dde72b0d0c37cce22d10b9c515552591a901
MrSurenK <surenkworkz@gmail.com>
2024-02-04 12:32:50 +0800

    Created instance variables with setters and getters in UserReview

--- backend/src/main/java/com/freshfeet/backend/model/UserReview.java
+++ backend/src/main/java/com/freshfeet/backend/model/UserReview.java
@@ -1,15 +1,42 @@
 package com.freshfeet.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class UserReview {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    private Integer rating;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String comment;
+
+
+    //Setters and Getters
+    public Long getId(){
+        return this.id;
+    }
+
+    public Integer getRating(){
+        return this.rating;
+    }
+
+    public void setRating(Integer rating){
+        this.rating = rating;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
+    public void setComment(String comment){
+        this.comment = comment;
+    }
+
 
 
 }
