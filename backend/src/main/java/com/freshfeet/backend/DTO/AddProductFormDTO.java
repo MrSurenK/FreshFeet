4cb693e0913f1906ba3fa641e2742853c8d3087b
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-02-11 13:46:50 +0800

    Defined AddProductFormDTO record

--- backend/src/main/java/com/freshfeet/backend/DTO/AddProductFormDTO.java
+++ backend/src/main/java/com/freshfeet/backend/DTO/AddProductFormDTO.java
@@ -1,12 +1,20 @@
 package com.freshfeet.backend.DTO;
 
-public class AddProductFormDTO {
+import java.math.BigDecimal;
 
-    private String name;
+// Record will automatically generate getters
+public record AddProductFormDTO(
+         String ProductName,
+         String description,
+         String productImage,
 
+         String productCategory,
 
+         String SKU,
 
+         BigDecimal price,
 
-
+         Integer qtyInStock
+) {
 
 }
