63d4e11116423e8f6cc9833ea65aa2938f8fac6c
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-02-13 01:04:52 +0800

    Created sql script to populate certain entities and adjusted hibernate settings accordingly

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO variation(name) VALUES('colours');
+INSERT INTO variation(name) VALUES('shoe_sizes');
+INSERT INTO variation_option(value, fk_variation_id) VALUES("red",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("orange",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("yellow",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("green",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("blue",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("purple",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("black",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("white",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("grey",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("pink",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("brown",1);
+INSERT INTO variation_option(value, fk_variation_id) VALUES("white",1);
+
+
+-- SELECT * FROM variation_option;
+-- SELECT * 
+-- FROM variation
+-- RIGHT JOIN variation_option ON variation.id = variation_option.fk_variation_id;
