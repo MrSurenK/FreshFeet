033dbeccfdccea8abd9ec7e5c0a4e809fcd69b5d
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-02-13 12:44:57 +0800

    Modify logic to ensure that all fields are submitted or throw HTTP errors.

--- backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
+++ backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.freshfeet.backend.controller;
 import com.freshfeet.backend.DTO.ProductFormDTO;
 import com.freshfeet.backend.service.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,12 +17,17 @@ public class ProductController {
 
     @Autowired
     private ListingService listingService;
-    @PostMapping("/addProduct")
+    @PostMapping("/addproduct")
     @ResponseBody
     public ResponseEntity<Object> addListing(@RequestPart ProductFormDTO productForm,
                                              @RequestPart MultipartFile file
-                                     ) throws IOException { ProductFormDTO updatedDto = listingService.createListing(productForm, file);
-        return ResponseEntity.ok(updatedDto);
+                                     ) throws IOException {
+        if (productForm != null && !file.isEmpty()){
+            ProductFormDTO updatedDto = listingService.createListing(productForm, file);
+            return ResponseEntity.accepted().body("Product has been added!");
+        } else {
+            return ResponseEntity.badRequest().body("Missing form items");
+        }
 
     }
 }
