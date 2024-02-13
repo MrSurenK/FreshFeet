2c57e7571d56763fbf3ba6ae723a463b101bb34d
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-02-13 16:29:03 +0800

    included error custom http error message in applications.properties

--- backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
+++ backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
@@ -19,15 +20,16 @@ public class ProductController {
     private ListingService listingService;
     @PostMapping("/addproduct")
     @ResponseBody
+    //No need to check if productForm is not submitted because
     public ResponseEntity<Object> addListing(@RequestPart ProductFormDTO productForm,
                                              @RequestPart MultipartFile file
                                      ) throws IOException {
-        if (productForm != null && !file.isEmpty()){
-            ProductFormDTO updatedDto = listingService.createListing(productForm, file);
-            return ResponseEntity.accepted().body("Product has been added!");
+        if (file.isEmpty()){
+           throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Missing product image!");
         } else {
-            return ResponseEntity.badRequest().body("Missing form items");
+            ProductFormDTO updatedDto = listingService.createListing(productForm, file);
+            return ResponseEntity.status(HttpStatus.OK).body("Product has been added!");
         }
-
     }
 }
+
