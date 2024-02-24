a92b9317d0833b28ec83df89d75bc94414b03950
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-02-25 01:21:58 +0800

    Wrote sql to insert data into productCategory table

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,15 +13,16 @@ INSERT INTO variation_option(value, fk_variation_id) VALUES("pink",1);
 INSERT INTO variation_option(value, fk_variation_id) VALUES("brown",1);
 INSERT INTO variation_option(value, fk_variation_id) VALUES("white",1);
 
+INSERT INTO product_category(category_name) VALUES ("Men");
+INSERT INTO product_category(category_name) VALUES ("Women");
+INSERT INTO product_category(category_name,fk_parent_cat) VALUES ("Shoes",1);
+INSERT INTO product_category(category_name,fk_parent_cat) VALUES ("Shoes",2);
 
 -- SELECT * FROM variation_option;
 -- SELECT * 
 -- FROM variation
 -- RIGHT JOIN variation_option ON variation.id = variation_option.fk_variation_id;
 
-INSERT INTO product_category(category_name) VALUES ("Men");
-INSERT INTO product_category(category_name) VALUES ("Women");
-INSERT INTO product_category(category_name,fk_parent_cat) VALUES ("Shoes",1);
-INSERT INTO product_category(category_name,fk_parent_cat) VALUES ("Shoes",2);
+
 
 
