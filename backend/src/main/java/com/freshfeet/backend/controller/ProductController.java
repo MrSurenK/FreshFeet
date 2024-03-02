4caec67ac2659ed73ea5c79d17dfea3846e04e88
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-03-02 15:56:42 +0800

    Initialised controller to be used with Thymeleaf

---
+++ backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.freshfeet.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ProductController {
+
+
+}
