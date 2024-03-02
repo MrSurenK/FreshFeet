057967a069ed8822139e47e3408f74bb1ebb2cee
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-03-02 22:19:16 +0800

    Test set up with Thymeleaf

--- backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
+++ backend/src/main/java/com/freshfeet/backend/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.freshfeet.backend.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class ProductController {
-
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello Thymeleaf!");
+        return "index"; // This will render index.html Thymeleaf template
+    }
 
 }
