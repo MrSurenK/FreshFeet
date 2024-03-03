616e8ab896a670a92b294a3572d21f7a23cb14c6
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-03-03 18:15:52 +0800

    ToDo: Create drop down menu on click and hover effect

--- backend/src/main/resources/static/javascript/sidebar.js
+++ backend/src/main/resources/static/javascript/sidebar.js
@@ -1,6 +1,24 @@
-document.querySelectorAll(".menuItem").forEach(item => {
-  item.addEventListener("click", event => {
-    event.stopPropagation(); // Stop the event from bubbling up the DOM tree
-    alert(item.innerHTML);
-  });
-});
+// Javascript for sidebar navigation
+
+// Task 1: When clicked on a menuItem show drop down if submenus exist
+
+  // Get the div tag of a main menu item
+  const menuItems = document.getElementsByClassName("menuItem");
+
+  /* Assign a click event listener that will hide or show the
+     submenus within the menu items if it exists.
+  */
+  menuItems.addEventListener("click",()=>{
+    for(const item of menuItems){
+        if (item.children){
+            const dropdownItems = item.getElementsByClassName("dropdown");
+            for(const each of dropdownItems){
+                if(each.style.display == "none"){
+                    each.style.display == "block"
+                } else {
+                    each.style.display =="none"
+                }
+            }
+        }
+    }
+  })
