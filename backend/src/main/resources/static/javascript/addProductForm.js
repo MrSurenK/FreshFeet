d2555f0f9536348b19987430c62ca6132c642817
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-03-18 21:18:15 +0800

    Created cnoditional drop down for subcat with appropriate id.

--- backend/src/main/resources/static/javascript/addProductForm.js
+++ backend/src/main/resources/static/javascript/addProductForm.js
@@ -1,18 +1,29 @@
-// Select the main and subcategory select tags wrapping the options
-
+//Get hold of the main category select
 const mainCat = document.getElementById("mainCategory");
+
+//Get hold of the subcategory select
 const subCat = document.getElementById("subCategory");
 
-mainCat.addEventListener("change", function() {
-    const selectedMainCat = this.value;
-    const subCatOptions = subCat.options;
-//    const catJson = JSON.parse(subCatOptions.getAttribute("catJson"));
+//Add onChange event listener to mainCategory select tag
+mainCat.addEventListener("change", ()=> {
 
-    //Enable sub category select
+    /* On change I want to compare the selected cat id with the parent
+        cat id in subCat select tag.
+        - When changing mainCat tag remove disable from sub-cat select tag
+        - If the the parent cat exists on the select tag then show it in the drop-down meny.
+    */
     subCat.disabled = false;
 
-    //Based on what is selected in main category show the valid options in subcategory
-    for(const i; i < subCatOptions.length; i++ ){
-    const matchId = subCatOptions[i].getAttribute('data-parentCat');
+    const selectedMainCat = mainCat.value;
+    const subCatOptions = subCat.options;
+
+    for (let i = 0; i < subCatOptions.length; i++ ){
+        if (selectedMainCat === subCatOptions[i].getAttribute("fk-data")){
+           subCatOptions[i].style.display="";
+        } else {
+           subCatOptions[i].style.display="none";
+    }
     }
 })
+
+
